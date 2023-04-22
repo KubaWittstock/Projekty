1b45b712aa266c6439802eecd953ff9a145781d2
KubaWittstock <91993149+KubaWittstock@users.noreply.github.com>
2023-04-22 15:16:18 +0200

    Po dodaniu bazy danych

--- django_tutorial/blog/admin.py
+++ django_tutorial/blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post, Tag, Image, Author
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("date_created", 'author', 'tags')
+    list_display = ("title", "date_created", 'author', 'last_modified',)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("first_name", 'last_name',)
+    list_display = ("first_name", 'last_name',)
+
+
+class TagAmdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("display_name", )}
+    list_display = ("display_name",)
+
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAmdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Image)
